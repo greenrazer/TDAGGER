d94fdf51649c3536aaf9e094d5b6b42309e6255d
keighbee <kb@huggingface.co>
2025-02-04 17:05:05 -0800

    flatten order tests

---
+++ src/ir/safe_ir/ops/ungroup_type.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Dict, List, Tuple, Union, Set
+
+from .op_type import OpType
+    
+
+@dataclass
+class UngroupSpec:
+    ungroup: Dict[int, List[int]]
+
+class UngroupType(OpType):
+    spec: UngroupSpec
+
+    def __init__(self, name, inputs, spec: UngroupSpec, debug_sources=[]):
+        super().__init__(name, inputs, debug_sources=debug_sources)
+        self.spec = spec
+
+    def __str__(self) -> str:
+        inp_name = self.inputs["input"]
+        out = f"%{self.name}: %{inp_name}[{self.spec}]{self.debug_sources_to_str()}"
+        return out
+
+    @property
+    def type(self) -> str:
+        return "ungroup"
+
+    @property
+    def required_input_keys(self) -> List[str]:
+        return ["input"]
