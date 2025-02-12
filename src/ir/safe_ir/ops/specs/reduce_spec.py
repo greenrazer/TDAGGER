76bbb3d51feb6c90c54e4fd6f1cca3566766f811
keighbee <kb@huggingface.co>
2025-02-12 13:26:53 -0800

    added shape, compute for reduce

--- src/ir/safe_ir/ops/specs/reduce_spec.py
+++ src/ir/safe_ir/ops/specs/reduce_spec.py
@@ -1,12 +1,13 @@
+import math
 from dataclasses import dataclass
 from enum import Enum, auto
 from typing import Any, Dict, List, Set, Tuple, Type, Union
 
+from ....compute_stats import ComputeStats
+from ...safe_ir import ScalarSpec, SpecType, TensorSpec
 from ..inputs.op_input import OpInput
 from ..inputs.unary_tensor_input import UnaryTensorInput
 from .op_spec import OpSpec
-from ...safe_ir import SpecType, TensorSpec, ScalarSpec
-from ....compute_stats import ComputeStats
 
 
 @dataclass
@@ -68,7 +69,72 @@ class ReduceSpec(OpSpec):
         return f"{' '.join(out)} | reduction={self.type}"
 
     def output_spec(self, inputs: List[SpecType]) -> SpecType:
-        pass
-    
+        real_indices = {
+            (idx if idx >= 0 else len(inputs[0].shape) + idx) for idx in self.dimensions
+        }
+        seen = {idx: False for idx in real_indices}
+
+        out_shape = []
+        for i, size in enumerate(inputs[0].shape):
+            if i in real_indices:
+                out_shape.append(1)
+                seen[i] = True
+            else:
+                out_shape.append(size)
+
+        if not all(seen.values()):
+            raise Exception(
+                f"shape not sufficient for reduction spec: {inputs[0].shape}."
+            )
+
+        return TensorSpec(shape=out_shape, data_type=inputs[0].data_type)
+
     def compute_stats(self, inputs: List[SpecType]) -> ComputeStats:
-        pass
+        real_indices = {
+            (idx if idx >= 0 else len(inputs[0].shape) + idx) for idx in self.dimensions
+        }
+
+        out_spec = self.output_spec(inputs)
+        out_size = out_spec.size()
+
+        reduce_shape = [s for i, s in enumerate(inputs[0].shape) if i in real_indices]
+        reduce_size = math.prod(reduce_shape)
+        match self.reduction_type:
+            case ReduceSpec.ReductionType.SUM:
+                return ComputeStats(
+                    flops=reduce_size,  # an addition for every reduced element
+                    reads=inputs[0].size(),  # must read every input once
+                    # a write for every out size and one for every addition
+                    writes=out_size * reduce_size,
+                )
+            case ReduceSpec.ReductionType.PRODUCT:
+                return ComputeStats(
+                    flops=reduce_size,  # a multiplication for every reduced element
+                    reads=inputs[0].size(),  # must read every input once
+                    # a write for every out size and one for every multiplication
+                    writes=out_size * reduce_size,
+                )
+            case ReduceSpec.ReductionType.MINIMUM:
+                return ComputeStats(
+                    flops=reduce_size,  # a comparison for every reduced element
+                    reads=inputs[0].size(),  # must read every input once
+                    # a write for every out size and one for the minimum
+                    writes=out_size + 1,
+                )
+            case ReduceSpec.ReductionType.MAXIMUM:
+                return ComputeStats(
+                    flops=reduce_size,  # a comparison for every reduced element
+                    reads=inputs[0].size(),  # must read every input once
+                    # a write for every out size and one for the maximum
+                    writes=out_size + 1,
+                )
+            case ReduceSpec.ReductionType.MEAN:
+                return ComputeStats(
+                    flops=reduce_size
+                    + 1,  # an addition for every reduced element and one division
+                    reads=inputs[0].size(),  # must read every input once
+                    # a write for every out size and one for every addition and one for the division
+                    writes=out_size * reduce_size + 1,
+                )
+            case _:
+                assert False, "ReductionType not defined"
---
+++ tests/ir/safe_ir/ops/specs/test_reduce_spec.py
@@ -0,0 +1,110 @@
+import math
+import unittest
+
+import torch
+import torch.nn as nn
+
+from src.ir.safe_ir import DataType, ReduceSpec, TensorSpec
+
+
+class TestReduceOutputSpec(unittest.TestCase):
+    def test_basic(self):
+        spec = ReduceSpec(
+            dimensions={1, 2}, reduction_type=ReduceSpec.ReductionType.SUM
+        )
+
+        input_specs = [TensorSpec(shape=(5, 2, 3, 4), data_type=DataType.FLOAT32)]
+
+        out_spec = spec.output_spec(input_specs)
+
+        self.assertEqual(out_spec.shape, [5, 1, 1, 4])
+        self.assertEqual(out_spec.data_type, DataType.FLOAT32)
+
+    def test_negitive_dimensions(self):
+        spec = ReduceSpec(
+            dimensions={-3, -2}, reduction_type=ReduceSpec.ReductionType.SUM
+        )
+
+        input_specs = [TensorSpec(shape=(5, 2, 3, 4), data_type=DataType.FLOAT32)]
+
+        out_spec = spec.output_spec(input_specs)
+
+        self.assertEqual(out_spec.shape, [5, 1, 1, 4])
+        self.assertEqual(out_spec.data_type, DataType.FLOAT32)
+
+    def test_raises_on_insufficient_shape(self):
+        spec = ReduceSpec(
+            dimensions={3, 4}, reduction_type=ReduceSpec.ReductionType.SUM
+        )
+
+        input_specs = [TensorSpec(shape=(5, 2, 3, 4), data_type=DataType.FLOAT32)]
+
+        with self.assertRaises(Exception):
+            spec.output_spec(input_specs)
+
+
+class TestReduceComputeStats(unittest.TestCase):
+    def test_sum(self):
+        spec = ReduceSpec(
+            dimensions={1, 2}, reduction_type=ReduceSpec.ReductionType.SUM
+        )
+
+        input_specs = [TensorSpec(shape=(5, 2, 3, 4), data_type=DataType.FLOAT32)]
+
+        out_spec = spec.compute_stats(input_specs)
+
+        self.assertEqual(out_spec.flops, 6)
+        self.assertEqual(out_spec.reads, math.prod(input_specs[0].shape))
+        self.assertEqual(out_spec.writes, math.prod(input_specs[0].shape))
+
+    def test_prod(self):
+        spec = ReduceSpec(
+            dimensions={1, 2}, reduction_type=ReduceSpec.ReductionType.PRODUCT
+        )
+
+        input_specs = [TensorSpec(shape=(5, 2, 3, 4), data_type=DataType.FLOAT32)]
+
+        out_spec = spec.compute_stats(input_specs)
+
+        self.assertEqual(out_spec.flops, 6)
+        self.assertEqual(out_spec.reads, math.prod(input_specs[0].shape))
+        self.assertEqual(out_spec.writes, math.prod(input_specs[0].shape))
+
+    def test_min(self):
+        spec = ReduceSpec(
+            dimensions={1, 2}, reduction_type=ReduceSpec.ReductionType.MINIMUM
+        )
+
+        input_specs = [TensorSpec(shape=(5, 2, 3, 4), data_type=DataType.FLOAT32)]
+
+        out_spec = spec.compute_stats(input_specs)
+
+        self.assertEqual(out_spec.flops, 6)
+        self.assertEqual(out_spec.reads, math.prod(input_specs[0].shape))
+        self.assertEqual(out_spec.writes, 21)
+
+    def test_max(self):
+        spec = ReduceSpec(
+            dimensions={1, 2}, reduction_type=ReduceSpec.ReductionType.MAXIMUM
+        )
+
+        input_specs = [TensorSpec(shape=(5, 2, 3, 4), data_type=DataType.FLOAT32)]
+
+        out_spec = spec.compute_stats(input_specs)
+
+        self.assertEqual(out_spec.flops, 6)
+        self.assertEqual(out_spec.reads, math.prod(input_specs[0].shape))
+        self.assertEqual(out_spec.writes, 21)
+
+    def test_mean(self):
+        spec = ReduceSpec(
+            dimensions={1, 2}, reduction_type=ReduceSpec.ReductionType.MEAN
+        )
+
+        input_specs = [TensorSpec(shape=(5, 2, 3, 4), data_type=DataType.FLOAT32)]
+
+        out_spec = spec.compute_stats(input_specs)
+
+        self.assertEqual(out_spec.flops, 6 + 1)
+        self.assertEqual(out_spec.reads, math.prod(input_specs[0].shape))
+        self.assertEqual(out_spec.writes, math.prod(input_specs[0].shape) + 1)
